97ed6e1546ac5f2fe4ed72cf2a9e95361f639946
Armin Ronacher <armin.ronacher@active-4.com>
2025-03-25 20:57:38 +0100

    Implement Future for sticky

--- src/lib.rs
+++ src/lib.rs
@@ -97,11 +97,10 @@
 //! be enabled which optimizes the internal storage of the [`Sticky`] type to
 //! make it use a [`slab`](https://docs.rs/slab/latest/slab/) instead.
 //!
-//! When turning on the `future` feature, then [`Fragile`] implements the
+//! When turning on the `future` feature, then the containers implement the
 //! [`Future`](std::future::Future) crate from the standard library to
 //! automatically wrap futures.  The `stream` crate does the same for the
-//! `future_core::Stream` type.  Note that this is only available for [`Fragile`]
-//! and none of the other types.
+//! `future_core::Stream` type.
 mod errors;
 mod fragile;
 mod registry;
